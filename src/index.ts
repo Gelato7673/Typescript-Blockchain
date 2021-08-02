cfd2d775236f130dfec07027ba0d7966ab8ed7c0
Aesoo Cho <aesoocho@aesoos-MacBook-Pro.local>
2021-08-02 10:17:41 +0900

    nomad lecture from 1 to 5

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const Name = "Aesoo Cho",
+age = 33,
+gender = "femaile"
+
+const sayHi = (name: string, age: number, gender:string):string => {
+  return`Hello, my name is ${name}. I'm ${age}years old. and I'm ${gender}!`;
+}
+
+console.log(sayHi('jinsook hong', 66, 'female'));
+
+export {};
+
+//6강 들을 차례임
